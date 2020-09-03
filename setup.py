540266d2f18940aa8b10c11eda822299dd0b83da
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-09-03 15:15:42 -0600

    Added package directory and setup file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""PyPan: The greatest thing since PAN AIR"""
+
+from setuptools import setup
+import os
+import sys
+
+setup(name = 'PyPan',
+    version = '1.0.0',
+    description = "PyPan: The greatest thing since PAN AIR",
+    url = 'https://github.com/usuaero/PyPan',
+    author = 'usuaero',
+    author_email = 'doug.hunsaker@usu.edu',
+    install_requires = ['numpy>=1.18', 'scipy>=1.4', 'pytest', 'matplotlib'],
+    python_requires ='>=3.6.0',
+    license = 'MIT',
+    packages = ['pypan'],
+    zip_safe = False)
