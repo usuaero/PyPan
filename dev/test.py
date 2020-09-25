7517e46ddda6e7266ac8e5955b5f74b76d1ecd45
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-09-24 18:00:40 -0600

    Working on centroid calculation.

--- dev/test.py
+++ dev/test.py
@@ -4,4 +4,5 @@ import numpy as np
 if __name__=="__main__":
 
     # Load mesh
-    my_mesh = pp.Mesh(mesh_file="dev/swept_wing.stl", mesh_file_type="STL", verbose=True)
+    my_mesh = pp.Mesh(mesh_file="dev/swept_wing_10.stl", mesh_file_type="STL", verbose=True)
+    my_mesh.plot()
--- pypan/geometry.py
+++ pypan/geometry.py
@@ -4,6 +4,9 @@ import stl
 import warnings
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+from mpl_toolkits.mplot3d import Axes3D
 
 
 class Panel:
@@ -26,27 +29,36 @@ class Tri(Panel):
 
         # Store vectors
         self.v0 = v0
-        self.v1 = v2
+        self.v1 = v1
         self.v2 = v2
-        print(self.v0)
-        print(self.v1)
-        print(self.v2)
         self.n = n
 
-        # Check normal
+        # Get normal vector
         if self.n is None:
-            d1 = self.v1-self.v0
-            d2 = self.v2-self.v1
-            N = np.cross(d1, d2)
-            self.n = N/np.linalg.norm(N)
+            self._calc_normal()
         else:
-            self.n = self.n/np.linalg.norm(self.n)
+            try:
+                self.n = self.n/np.linalg.norm(self.n)
+            except RuntimeWarning:
+                self._calc_normal()
 
-        # Calculate area
+        # Determine area
+        self._calc_area()
+
+        # Determine centroid
+        self._calc_centroid()
+
+
+    def _calc_area(self):
+        # Calculates the panel area
+
+        # Get vector components
         nx, ny, nz = self.n
         x0, y0, z0 = self.v0
         x1, y1, z1 = self.v1
         x2, y2, z2 = self.v2
+
+        # Get the area using Stoke's theorem
         dA0 = 0.5*ny*(z1+z0)*(x1-x0)\
              +0.5*nz*(x1+x0)*(y1-y0)\
              +0.5*nx*(y1+y0)*(z1-z0)
@@ -56,10 +68,48 @@ class Tri(Panel):
         dA2 = 0.5*ny*(z0+z2)*(x0-x2)\
              +0.5*nz*(x0+x2)*(y0-y2)\
              +0.5*nx*(y0+y2)*(z0-z2)
-        self.A = dA0+dA1+dA2
-        print(self.A)
 
-        # Calculate centroid
+        self.A = dA0+dA1+dA2
+
+
+    def _calc_normal(self):
+        # Calculates the panel unit normal vector
+        d1 = self.v1-self.v0
+        d2 = self.v2-self.v1
+        N = np.cross(d1, d2)
+        self.n = N/np.linalg.norm(N)
+
+    
+    def _calc_centroid(self):
+        # Calculates the location of the panel centroid
+
+        # Get primed coordinate system
+        x_p = self.v1-self.v0
+        x_p = x_p/np.linalg.norm(x_p)
+        z_p = np.copy(self.n)
+        y_p = np.cross(z_p, x_p)
+
+        # Construct transformation matrix
+        T = np.concatenate((x_p[:,np.newaxis], y_p[:,np.newaxis], z_p[:,np.newaxis]), axis=1).T
+        
+        # Transform vertices
+        v0_p = np.matmul(T, self.v0[:,np.newaxis]).flatten()
+        v1_p = np.matmul(T, self.v1[:,np.newaxis]).flatten()
+        v2_p = np.matmul(T, self.v2[:,np.newaxis]).flatten()
+
+        # Get transformed coordinates of centroid
+        x_c_p = 1.0/(6.0*self.A)*(
+            (v0_p[0]+v1_p[0])*(v0_p[0]*v1_p[1]-v1_p[0]*v0_p[1])+
+            (v1_p[0]+v2_p[0])*(v1_p[0]*v2_p[1]-v2_p[0]*v1_p[1])+
+            (v2_p[0]+v0_p[0])*(v2_p[0]*v0_p[1]-v0_p[0]*v2_p[1]))
+        y_c_p = 1.0/(6.0*self.A)*(
+            (v0_p[1]+v1_p[1])*(v0_p[0]*v1_p[1]-v1_p[0]*v0_p[1])+
+            (v1_p[1]+v2_p[1])*(v1_p[0]*v2_p[1]-v2_p[0]*v1_p[1])+
+            (v2_p[1]+v0_p[1])*(v2_p[0]*v0_p[1]-v0_p[0]*v2_p[1]))
+
+        # Transform back to standard coordinates
+        v_cg_p = np.array([x_c_p, y_c_p, 0.0])[:,np.newaxis]
+        self.v_cg = np.matmul(T.T, v_cg_p).flatten()
 
 
 class Mesh:
@@ -98,7 +148,7 @@ class Mesh:
         # Initialize panel objects
         self.N = self._raw_stl_mesh.v0.shape[0]
         if self._verbose: print("Successfully read STL file with {0} facets.".format(self.N))
-        self.panels = np.zeros(self.N, dtype=Tri)
+        self.panels = np.empty(self.N, dtype=Tri)
         for i in range(self.N):
             self.panels[i] = Tri(self._raw_stl_mesh.v0[i],
                                  self._raw_stl_mesh.v1[i],
@@ -115,3 +165,42 @@ class Mesh:
     def _check_mesh(self):
         # Checks the mesh is appropriate and determines where Kutta condition should exist
         pass
+
+
+    def plot(self, **kwargs):
+        """Plots the mesh in 3D.
+
+        Parameters
+        ----------
+        vertices : bool, optional
+            Whether to display vertices. Defaults to True.
+
+        centroids : bool, optional
+            Whether to display centroids. Defaults to True.
+        """
+
+        # Set up plot
+        fig = plt.figure(figsize=plt.figaspect(1.0))
+        ax = fig.gca(projection='3d')
+        
+        # Plot vertices
+        if kwargs.get("vertices", True):
+            for i in range(self.N):
+                panel = self.panels[i]
+                ax.plot(panel.v0[0], panel.v0[1], panel.v0[2], 'b.')
+                ax.plot(panel.v1[0], panel.v1[1], panel.v1[2], 'b.')
+                ax.plot(panel.v2[0], panel.v2[1], panel.v2[2], 'b.')
+        
+        # Plot centroids
+        if kwargs.get("centroids", True):
+            for i in range(self.N):
+                panel = self.panels[i]
+                ax.plot(panel.v_cg[0], panel.v_cg[1], panel.v_cg[2], 'r.')
+
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        ax.set_xlim3d(5.0, -5.0)
+        ax.set_ylim3d(-5.0, 5.0)
+        ax.set_zlim3d(5.0, -5.0)
+        plt.show()
