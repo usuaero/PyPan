b4af627e6206afcf317f790f629aaaac5f0db4c7
Cory Goates <cory.goates@aggiemail.usu.edu>
2020-11-12 15:26:22 -0700

    More meshes!

--- dev/test.py
+++ dev/test.py
@@ -15,9 +15,10 @@ if __name__=="__main__":
     #mesh_file = "dev/meshes/5000_polygon_sphere_100mm.STL"
     #mesh_file = "dev/meshes/20000_polygon_sphere_100mm.STL"
     #mesh_file = "dev/meshes/1250_sphere.vtk"
-    mesh_file = "dev/meshes/swept_wing_21_rounded.stl"
+    #mesh_file = "dev/meshes/swept_wing_21_rounded.stl"
     #mesh_file = "dev/meshes/swept_wing_21.vtk"
     #mesh_file = "dev/meshes/F16_Original_withFins.vtk"
+    mesh_file = "dev/meshes/swept_wing_30_span_31_sec_10_tip.stl"
     start_time = time.time()
 
     my_mesh = pp.Mesh(mesh_file=mesh_file, mesh_file_type="STL", kutta_angle=90.0, verbose=True)
